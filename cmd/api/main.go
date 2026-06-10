d10e9fd9258f74f05d6e41cad00c75b1f2839666
mukuvi <jamesmngandu@gmail.com>
2026-06-10 13:50:45 +0300

    commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,11 +12,22 @@ type Course struct {
 
 }
 func main(){
-	http.HandleFunc("/", homeRoute)
-	http.ListenAndServe(":8080",nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", homeRoute)
+	mux.HandleFunc("/courses", ourCourses)
+	http.ListenAndServe(":8080", mux)
+	
 }
 
 func homeRoute(w http.ResponseWriter, r *http.Request){
 	var welcome ="welcome to the lang infrastructure"
 	fmt.Fprintln(w, welcome)
+}
+
+func ourCourses(w http.ResponseWriter, r *http.Request){
+  oc, err := w.Write([]byte("hey"))
+  if err != nil{
+	panic(err)
+  }
+  fmt.Println(oc)
 }
