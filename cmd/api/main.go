b7ecfee8a9388fb2759e9f3eaa712be189bc57b7
mukuvi <jamesmngandu@gmail.com>
2026-06-10 12:37:00 +0300

    commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Course struct {
 	Name string `json:"coursename"`
 	Price int `json:"courseprice"`
@@ -7,5 +12,11 @@ type Course struct {
 
 }
 func main(){
-	
+	http.HandleFunc("/", homeRoute)
+	http.ListenAndServe(":8080",nil)
+}
+
+func homeRoute(w http.ResponseWriter, r *http.Request){
+	var welcome ="welcome to the lang infrastructure"
+	fmt.Fprintln(w, welcome)
 }
